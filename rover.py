50ad1bcac39b3660d49b937162005723fb884444
Bryan Mah <bryan.mah1@marist.edu>
2018-09-06 15:00:08 -0400

    Solution to Curiosity rover exercise.

---
+++ rover.py
@@ -0,0 +1,11 @@
+# CMPT 120L 113
+# Mars Rover Program
+# Author: Bryan Mah
+# Created: 6 Sep 2018
+
+def main():
+    time = (34000000/186000)/60
+    print("It takes", time, "minutes for a photo from Curioisity to reach NASA")
+    
+main()
+
