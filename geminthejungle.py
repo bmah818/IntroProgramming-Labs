c4cb7aa4a7227e413dfba86837ed1c26f1908b43
Bryan Mah <bryan.mah1@marist.edu>
2018-09-12 18:47:29 -0400

    Edited storyline and added player customization

---
+++ geminthejungle.py
@@ -0,0 +1,94 @@
+# CMPT 120L 113
+# Bryan Mah
+# 12 Sep 2018
+###
+
+# show game title
+print("\nGem in the Jungle"
+      "\n========\n")
+
+# show game introduction
+print(" During your childhood, you always had a passion for exploring. Over"
+      " the past five years, you spent your time in Egypt, exploring hidden"
+      " tunnels inside pyramids. You knew there would be a room full of"
+      " treasures, and you wanted to find it so you could become rich."
+      " You were able to find the room full of treasures, and you found a"
+      " diamond that was double the size of diamonds you would normally find."
+      " You quickly hid it in your backpack, not wanting anyone to see what you"
+      " found. Days pass and you get the diamond appraised. It's worth $100"
+      " million. You quickly make an excuse to your team that helped uncover"
+      " the treasure room and you board a flight to New York, where you are"
+      " planning to auction it. After a couple hours into the flight, the pilots"
+      " lose control and the plane comes diving down into a deserted island"
+      " and you lose consciousness...")
+
+# prompt the user
+input("\n<Press Enter to continue...>\n")
+
+# show the current location
+print("You wake up to the sounds of people chanting. "
+          "You see the debris of the plane not too far from you. "
+          "You realize the backpack with the diamond is not next to you anymore. ")
+
+# show the current score
+print("Your score is 0.")
+
+# prompt the user
+input("\n<Press Enter to continue...>\n")
+
+# show the current location
+print("You move away from the natives as they are carrying spears and shields"
+      "while surrounding the plane. It is getting dark and it looks like it"
+      "is going to rain, so you start making a shelter using sticks and"
+      "extremely thick and large leaves. As night arrives, you fall asleep,"
+      "hoping that you won't be discovered by the natives.")
+
+# show the current score
+print("Your score is 5.")
+
+# same as above
+input("\n<Press Enter to continue...>\n")
+
+# same as above
+print("As you awake, you notice that the natives have left the plane debris"
+      " so you decide to carefully visit the crash site. On your way to the plane,"
+      " you see a cargo ship passing by. You try to get it's attention but no one "
+      " notices you. When you finally reach the plane, you manage to find a working "
+      " flare gun and your backpack with the gem in it, unscathed.")
+
+# same as above
+print("Your score is 10.")
+
+# again...
+input("\n<Press Enter to continue...>\n")
+
+# again...
+print("You decide to need a weapon to defend yourself against the natives, or"
+      " even any animals that attempt to harm you. You manage to find a small"
+      " piece of sharp metal and some duct tape. You use the tape one end of"
+      " the metal and use that as a handle for your makeshift knife.")
+
+# again...
+print("Your score is 15.")
+
+# yet again...
+input("\n<Press Enter to continue...>\n")
+
+# yet again...
+print("As you return to your makeshift home, you encounter a wolf and a"
+      " native who has found your home. You manage to take both out using"
+      " the makeshift knife. You discard the remains into the oceanso no"
+      " one can find the remains."
+
+# yet again...
+print("Your score is 20.")
+
+# one last time...
+input("\n<Press Enter to continue...>\n")
+
+# show game ending
+print("As you enter your home, you hear a dozen natives yelling at you while"
+      " aiming their spears at you. Game over!")
+
+# show credits
+print("\nCopyright (c) 2018 Bryan Mah, bryan.mah1@marist.edu")
