fcdfd9500ef2326f0e9aa46bb2d7fd73e6864eab
Bryan Mah <bryan.mah1@marist.edu>
2018-09-13 15:10:43 -0400

    Solution to madlibs exercise

--- madlib.py
+++ madlib.py
@@ -1,15 +1,20 @@
 # CMPT 120L 113
 # Madlib Program
 # Author: Bryan Mah
-# Created 9 Sep 2018
+# Created 13 Sep 2018
 
-def main():
+def promptForWords():
+    global noun, verb, adjective, place
     noun = input("Enter a noun: ")
     verb = input("Enter a verb: ")
     adjective = input("Enter an adjective: ")
     place = input("Enter a place: ")
-    print("The "+ adjective +" " + noun + " " + verb + " to the" " " + place + " today.")
+
+def makeAndPrintSentence():
+    print("The "+ adjective + " " + noun + " " + verb + " to the" + place + " today.")
+    
+def main():
+    promptForWords()
+    makeAndPrintSentence()
 
 main()
-
-
