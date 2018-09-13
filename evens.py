b9405f4f05cf770bda041707b25fb8cd84a5826b
Bryan Mah <bryan.mah1@marist.edu>
2018-09-13 14:43:03 -0400

    small changes

--- evens.py
+++ evens.py
@@ -1,4 +1,10 @@
-for i in range(2,22,2):
-    print(i)
+# CMPT 120L 113
+# Evens Program
+# Author: Bryan Mah
+# Created: 13 Sep 2018
 
+def main():
+    for i in range(2,22,2):
+        print(i)
 
+main()
