9afc417dff8c980fd0a9c73a89dc1b7e36223f30
Bryan Mah <bryan.mah1@marist.edu>
2018-09-13 14:37:13 -0400

    Solution to Mad Libs Exercise.

---
+++ madlib.py
@@ -0,0 +1,15 @@
+# CMPT 120L 113
+# Madlib Program
+# Author: Bryan Mah
+# Created 9 Sep 2018
+
+def main():
+    noun = input("Enter a noun: ")
+    verb = input("Enter a verb: ")
+    adjective = input("Enter an adjective: ")
+    place = input("Enter a place: ")
+    print("The "+ adjective +" " + noun + " " + verb + " to the" " " + place + " today.")
+
+main()
+
+
